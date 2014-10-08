46f73749e2d4f7dc91d29ef343e2718e00874b3a
root <root@gitolite-demo2.(none)>
2014-10-08 00:28:00 +0000

    Automatic merge (git-subtree add/merge) by /usr/share/gitted/export/git-subtree on gitolite-demo2

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 . /usr/lib/sysconf.base/common.sh
 
+INSTALL_CGIT_URL=git://hjemli.net/pub/git/cgit
+
 if grep -q "^AcceptEnv LANG LC_\*$" /etc/ssh/sshd_config; then
     # Avoid the messages like: "perl: warning: Setting locale failed."
     # This is a minimal system, we just need the "C" locale
@@ -18,5 +20,36 @@ grep -q ^git: /etc/passwd || {
     cd /var/lib/git
     sudo -u git gitolite setup -pk $dir/bootstrap.admin.key.pub
 }
+gitolite_repositories=/var/lib/git/repositories
 
 sysconf_require_packages lighttpd
+
+if true; then
+    # Install cgit
+    if [ ! -f /usr/share/cgit/cgit.cgi ]; then
+        nef_log "Building cgit from: $INSTALL_CGIT_URL"
+        sysconf_require_packages libssl-dev gcc make
+        tmp_dir=$(mktemp -d)
+        git clone $INSTALL_CGIT_URL $tmp_dir
+        cd $tmp_dir
+        make get-git
+        make
+        mkdir /usr/share/cgit
+        cp cgit.css cgit.png /usr/share/cgit
+        cp cgit /usr/share/cgit/cgit.cgi
+        cd /
+        rm -rf $tmp_dir
+        apt-get remove --yes libssl-dev gcc make
+        apt-get autoremove --yes
+
+        cat <<EOF >/etc/cgitrc.d/generated.cgitrc
+root-title=Local Git repositories on $(hostname)
+root-desc=managed by Gitted/gitolite in $gitolite_repositories
+scan-path=$gitolite_repositories
+EOF
+        sysconf-etc.d update cgitrc
+        rm -f /etc/lighttpd/lighttpd.conf
+        ln -s cgit.lighttpd.conf /etc/lighttpd/lighttpd.conf
+        service lighttpd restart
+    fi
+fi
